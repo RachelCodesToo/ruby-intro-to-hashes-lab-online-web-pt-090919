1ca17053bb9ec39175a7cb843b57507b7a62ed26
Anne Fausto <a.r.fausto@gmail.com>
2019-09-27 09:36:18 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,7 +19,6 @@ end
 def monopoly_with_third_tier
   monopoly = {:railroads => {:pieces => 4}}
   monopoly_with_second_tier[:railroads][:names]={}
-  monopoly_with_third_tier
 end
 
 def monopoly_with_fourth_tier
